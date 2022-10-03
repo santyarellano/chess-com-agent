a1af484f5629ca6e41fd7e05f0b1557c91c6ea09
santyarellano <santy.arellano039@gmail.com>
2022-10-03 12:03:15 -0500

    minor fix and gitignore

--- src/chess_util.py
+++ src/chess_util.py
@@ -3,8 +3,9 @@ import random
 
 def get_random_legal_move(board: chess.Board):
     legal_moves = list(board.legal_moves)
-    move_selected = random.randint(0, len(legal_moves)-1)
-    return legal_moves[move_selected]
+    if len(legal_moves) > 0:
+        move_selected = random.randint(0, len(legal_moves)-1)
+        return legal_moves[move_selected]
 
 def get_square_location(location):
     x = ord(location[0]) - ord('a') + 1
